c058ac166196565999a4f9f1f5f868abcccb84ef
Saul Ceja <saulceja08@gmail.com>
2023-06-30 20:30:44 -0700

    Added Header, Main, and Footer components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import MainComponent from "./components/MainComponent";
+
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
+      <MainComponent />
+      <FooterComponent />
     </div>
   );
 }
