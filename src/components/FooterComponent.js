3030d02746272255d764b98d6ba2463aedf1ae1d
Saul Ceja <saulceja08@gmail.com>
2023-07-02 18:19:45 -0700

    add a apiResults test to make sure a fetch of the backend is working

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-function FooterComponent() {
+const FooterComponent = () => {
+  const testDeployedAPI = async () => {
+    try {
+      let apiResults = await fetch('https://saulceja08-flatiron-phase-2-json-server.onrender.com/toys')
+        .then(r => r.json())
+        .then(data => data);
+      console.log(apiResults);
+    } catch (error) {
+      console.log('did not work', error);
+    }
+  };
+
+  useEffect(() => {
+    testDeployedAPI();
+  }, []);
+
   return (
     <div>FooterComponent</div>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
