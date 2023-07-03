4a971fe9f54cedc757ded63ee7c42c6ebc676136
Saul Ceja <saulceja08@gmail.com>
2023-07-02 18:56:32 -0700

    fetch data from onrender.com json server and map all toy images and names

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,24 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const FooterComponent = () => {
-  const testDeployedAPI = async () => {
-    try {
-      let apiResults = await fetch('https://saulceja08-flatiron-phase-2-json-server.onrender.com/toys')
-        .then(r => r.json())
-        .then(data => data);
-      console.log(apiResults);
-    } catch (error) {
-      console.log('did not work', error);
-    }
-  };
+const MainComponent = () => {
+  const [toys, setToys] = useState([]);
 
   useEffect(() => {
-    testDeployedAPI();
+    // Fetch data from the API URL
+    fetch('https://saulceja08-flatiron-phase-2-json-server.onrender.com/toys')
+      .then((response) => {
+        // Check if the response is successful
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        // Parse the response as JSON
+        return response.json();
+      })
+      .then((data) => {
+        // Update the state with the fetched data
+        setToys(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
   return (
-    <div>FooterComponent</div>
+    <div>
+      <h1>Toys</h1>
+      <ul>
+        {toys.map((toy) => (
+          <li key={toy.id}>
+            <img src={toy.image} alt={toy.name} />
+            <h3>{toy.name}</h3>
+            <p>Likes: {toy.likes}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-export default FooterComponent;
+export default MainComponent;
