8c00197ed326b7765e50d64ff748fed315abb8c1
Saul Ceja <saulceja08@gmail.com>
2023-07-03 00:35:08 -0700

    change header text and formatting

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HeaderComponent = ({ searchTerm, onSearchChange }) => {
   return (
     <div className="header">
-      <h1>Toy Story Forum</h1>
+      <h1 className="toy-story-theme">Toy Story Forum</h1>
     </div>
   );
 };
